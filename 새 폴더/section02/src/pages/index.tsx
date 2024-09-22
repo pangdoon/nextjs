03ef5b701f48e14c9a16f39b290d407ba13c1fb3
pangdoon <kimyoung100@naver.com>
2024-09-22 13:36:41 +0900

    0922

--- 새 폴더/section02/src/pages/index.tsx
+++ 새 폴더/section02/src/pages/index.tsx
@@ -1,19 +1,40 @@
 import style from "./index.module.css";
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
-import books from "@/mock/books.json"
 import BookItem from "@/components/book-item";
+import { InferGetStaticPropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+export const getStaticProps = async() => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+
+    console.log("인덱스 페이지")
+
+  const [allBooks, recBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: {
+      allBooks,
+      recBooks,
+    },
+  };
+};
+
+export default function Home({allBooks,recBooks}: InferGetStaticPropsType<typeof getStaticProps>) {
+
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book)=> (<BookItem key={book.id} {...book}/>))}
+        {recBooks.map((book)=> (<BookItem key={book.id} {...book}/>))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book)=> (<BookItem key={book.id} {...book}/>))}
+        {allBooks.map((book)=> (<BookItem key={book.id} {...book}/>))}
       </section>
   </div>
   );
