c0484f5a4a28d798e4ecdbb3f39dd00f545a5af2
pangdoon <kimyoung100@naver.com>
2024-09-25 23:43:01 +0900

    0925

--- 새 폴더/section03/src/app/(with-searchbar)/page.tsx
+++ 새 폴더/section03/src/app/(with-searchbar)/page.tsx
@@ -1,9 +1,14 @@
+import ClientComponent from "./client-component";
 import styles from "./page.module.css";
+import ServerComponent from "./sever-component";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       인덱스페이지
+      <ClientComponent>
+        <ServerComponent/>
+      </ClientComponent>
     </div>
   );
 }
